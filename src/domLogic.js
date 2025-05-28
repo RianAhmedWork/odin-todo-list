ee92eb4523a2bc940562a21d2fa81a0f36c213d3
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-05-28 04:36:56 -0300

    store objects in local storage & work on display

--- src/domLogic.js
+++ src/domLogic.js
@@ -25,14 +25,16 @@ function addNewProject() {
             projectModal.close();
 
             const newProject = new Project(document.getElementById("project-name").value);
+            newProject.todos.push(new Todo("test", "This is something", "2025-01-01", "low"));
+            localStorage.setItem(newProject.name, JSON.stringify(newProject));
             const newProjectButton = document.createElement("button");
 
             newProjectButton.classList = "project-buttons"
-            newProjectButton.dataset.project = JSON.stringify(newProject);
             newProjectButton.textContent = newProject.name;
 
             newProjectButton.addEventListener("click", () => {
-                addTodoButton.dataset.project = JSON.stringify(newProject);
+                addTodoButton.dataset.project = newProject.name;
+                displayProject(newProject.name);
             });
 
             projectList.append(newProjectButton);
@@ -54,7 +56,11 @@ function addNewTodo() {
     const todoPriority = document.getElementById("todo-priority");
 
     addTodoButton.addEventListener("click", () => {
+        if (addTodoButton.dataset.project) {
         todoModal.showModal();
+        } else {
+            alert("Please select a project before adding a todo");
+        }
     });
 
     modalCloseButton.addEventListener("click", () => {
@@ -64,13 +70,38 @@ function addNewTodo() {
     modalSubmit.addEventListener("click", (event) => {
         event.preventDefault();
         if(todoForm.checkValidity()) {
-            const currentProject = JSON.parse(addTodoButton.dataset.project);
-            const parsedDate = parse(todoDue.value, "yyyy-MM-dd", new Date());
-            currentProject.addTodo(new Todo(todoName.value, todoDescription.value, parsedDate, todoPriority.value));
-            console.log(currentProject.todos[0]);
+            const retrievedProject = localStorage.getItem(addTodoButton.dataset.project);
+            const parsedProject = JSON.parse(retrievedProject);
+            const revivedProject = Object.assign(new Project(), parsedProject);
+            revivedProject.todos.forEach((item, index, array) => {
+                array[index] = Object.assign(new Todo(), item);
+            });
+            console.log(revivedProject instanceof Project);
+            console.log(revivedProject.todos[0] instanceof Todo);
+            // revivedProject.addTodo(new Todo(todoName.value, todoDescription.value, todoDue.value, todoPriority.value));
+            // console.log(revivedProject.todos[0]);
+            // revivedProject.todos[0].editTodo("A", "A", "2020-01-01", "medium");
+            // console.log(revivedProject.todos[0]);
         } else {
             alert("please fill out the todo form properly");
         }
         console.log(todoDue.value);
     });
+}
+
+function displayProject(projectName) {
+    const retrivedProject = localStorage.getItem(projectName);
+    const revivedProject = Object.assign(new Project(), retrivedProject);
+    revivedProject.todos.forEach((item, index, array) => {
+        array[index] = Object.assign(new Todo(), item);
+    });
+    const mainContent = document.getElementById("main-content");
+    const projectTitle = document.createElement("h1");
+    projectTitle.textContent = revivedProject.name;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete Project";
+    const contentHeading = document.createElement("div");
+    contentHeading.id = "content-heading";
+    contentHeading.append(projectTitle, deleteButton);
+    mainContent.appendChild(contentHeading);
 }
